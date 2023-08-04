f6c38955873a15ed599947fef958b3aa04ec3151
Ayush Kumar Singh <aksingh.er564@gmail.com>
2023-08-04 17:31:18 +0530

    adding database name to mongodb atlas

--- backened/db.js
+++ backened/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongodbURI = 'mongodb+srv://aksingher564:ayush123@cluster0.gyeqwz2.mongodb.net/?retryWrites=true&w=majority';
+const mongodbURI = 'mongodb+srv://aksingher564:ayush123@cluster0.gyeqwz2.mongodb.net/iNotebook?retryWrites=true&w=majority';
 function connecttomongodb() {
     mongoose.connect(mongodbURI).then(() => { 
         console.log('connected to mongodb server'); 
